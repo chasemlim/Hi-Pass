c89c0c87939c2f26d0e9135c9b650aee460c79c8
chasemlim <chasemlim@gmail.com>
2018-12-13 09:58:40 -0800

    Add more styling for landing-page-search-bar and begin work on modal

--- frontend/components/welcome_page/welcome_page.jsx
+++ frontend/components/welcome_page/welcome_page.jsx
@@ -6,12 +6,18 @@ class WelcomePage extends React.Component {
     render() {
         return (
             <div className="welcome-page">
-                <div className="search-bar">
-                    <form action="">
-                        <input className="welcome-search-bar" type="search" placeholder="Search for artists, bands, tracks, podcasts" />
-                        <i className="fas fa-search s-icon"></i>
-                    </form>
-                    
+                <div className="search-and-upload">
+                    <div className="search-bar">
+                        <form action="">
+                            <input className="welcome-search-bar" type="search" placeholder="Search for artists, bands, tracks, podcasts" />
+                            <i className="fas fa-search s-icon"></i>
+                        </form>
+
+                    </div>
+                    <div className="search-upload">
+                        <h2 className="welcome-or">or</h2>
+                        <button className="welcome-upload-own-button">Upload your own</button>
+                    </div>
                 </div>
             </div>
         )
